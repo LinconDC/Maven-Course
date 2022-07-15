4897f2d5896b126fe84efff74bccdbb4a9067053
Lincon Martins <linconmartins18@gmail.com>
2022-07-15 08:37:09 -0300

    Dependencies, build and configuration.

---
+++ loja/src/test/java/br/com/alura/loja/produtoTest.java
@@ -0,0 +1,14 @@
+package br.com.alura.loja;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class produtoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
